a8f8773ef29be150f274d1ef33256f73e68b0f44
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-14 17:20:17 +0200

    add class data

---
+++ screens/shop/ProductsOverViewScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+
+const ProductsOverViewScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default ProductsOverViewScreen
+
+const styles = StyleSheet.create({})
