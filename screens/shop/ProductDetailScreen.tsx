cdcc9b8971382a36a9dad351a37cf41a26e9dc20
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-15 22:34:58 +0200

    style detail screen

--- screens/shop/ProductDetailScreen.tsx
+++ screens/shop/ProductDetailScreen.tsx
@@ -8,8 +8,31 @@ import {
   ScrollView,
 } from "react-native";
 import { useSelector } from "react-redux";
+import Colors from "../../constants/Colors";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20,
+    fontFamily: "bold",
+  },
+  description: {
+    fontSize: 15,
+    textAlign: "center",
+    fontFamily: "regular",
+    marginHorizontal: 20,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center",
+  },
+});
 
 const ProductDetailScreen = (props: any) => {
   const productId = props.route.params.productId;
@@ -21,9 +44,14 @@ const ProductDetailScreen = (props: any) => {
     setOptions({ headerTitle: selectedProduct?.title });
   }, [setOptions]);
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add to Cart" onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}> {selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
