b346013d56edc064a38f20a36515a73df4b95ea0
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-18 21:40:12 +0200

    add update and create products logic in redux store

--- store/actions/products.ts
+++ store/actions/products.ts
@@ -1,5 +1,39 @@
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const CREATE_PRODUCT = "CREATE_PRODUCT";
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 export const deleteProduct = (productId: string) => {
   return { type: DELETE_PRODUCT, pid: productId };
+};
+export const createProduct = (
+  title: string,
+  description: string,
+  imageUrl: string,
+  price: string
+) => {
+  return {
+    type: CREATE_PRODUCT,
+    productData: {
+      title,
+      description,
+      imageUrl,
+      price,
+    },
+  };
+};
+export const updateProduct = (
+  id: string,
+  title: string,
+  description: string,
+  imageUrl: string
+) => {
+  return {
+    type: UPDATE_PRODUCT,
+    productData: {
+      title,
+      description,
+      imageUrl,
+    },
+    pid: id,
+  };
 };
