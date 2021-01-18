b71858d9a2577f83edea467ef3e7651f1f66e5e4
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-18 19:24:29 +0200

    add delete redux logic

--- store/reducers/products.ts
+++ store/reducers/products.ts
@@ -1,4 +1,5 @@
 import PRODUCTS from "../../data/dummy-data";
+import { DELETE_PRODUCT } from "../actions/products";
 
 const initialState: any = {
   availableProducts: PRODUCTS,
@@ -6,5 +7,17 @@ const initialState: any = {
 };
 
 export default (state = initialState, action: any) => {
+  switch (action.type) {
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        userProducts: state.userProducts.filter(
+          (product: any) => product.id !== action.pid
+        ),
+        availableProducts: state.availableProducts.filter(
+          (product: any) => product.id !== action.pid
+        ),
+      };
+  }
   return state;
 };
